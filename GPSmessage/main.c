eb2bcb4d62bb5b1668ff32134beae6533040bcd0
20161104591 <346680424@qq.com>
2017-06-22 11:57:22 +0800

    zuoye

--- GPSmessage/main.c
+++ GPSmessage/main.c
@@ -7,9 +7,24 @@
 //
 
 #include <stdio.h>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
-}
+#include <stdlib.h>
+#define N 10000
+int main()
+{
+        int i;
+        FILE *fp;
+        char str1[65];
+        char str2[65];
+        char lat[9];
+        fp=fopen("//Users//a20161104593//Desktop//gps//GPS170510.log","r+");
+        fscanf(fp,"%s%s",str1,str2);
+        printf("%s\n%s\n",str1,str2);
+        for(i=0;i<8;i++)
+        {
+            lat[i]=str1[i+16];
+        }
+        lat[8]='\0';
+        printf("纬度 :%s\n",lat);
+        fclose(fp);
+        return 0;
+    }
---
+++ input.h
@@ -0,0 +1,14 @@
+//
+//  input.h
+//  GPSmessage
+//
+//  Created by 20161104591 on 17/6/19.
+//  Copyright © 2017年 20161104591. All rights reserved.
+//
+
+#ifndef input_h
+#define input_h
+
+#include <stdio.h>
+
+#endif /* input_h */
