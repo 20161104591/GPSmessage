ae7e64d296ff913f34975e97e66cb438bae23226
20161104591 <346680424@qq.com>
2017-06-27 12:01:43 +0800

    表格

--- GPSmessage/main.c
+++ GPSmessage/main.c
@@ -25,7 +25,7 @@ int main()
         char num[3];        //卫星数量
         char high[5];       //海拔高度
         fp=fopen("//Users//a20161104591//Desktop//GPSmessage//GPS170510.txt","r+");
-        fw=fopen("//Users//a20161104591//Desktop//GPSmessage//output.txt","w+");
+        fw=fopen("//Users//a20161104591//Desktop//GPSmessage//output.csv","w+");
    while(fscanf(fp,"%s%s",str1,str2)!=EOF)
     {
         printf("%s\n%s\n",str1,str2);
@@ -42,49 +42,49 @@ int main()
         }
         lat[8]='\0';
         printf("纬度 :%s\n",lat);
-        fprintf(fw,"纬度 :%s\n",lat);
+        fprintf(fw,"纬度 :%s,",lat);
         for(i=0;i<9;i++)
         {
             lat2[i]=str1[i+27];
         }
         lat2[9]='\0';
         printf("经度 :%s\n",lat2);
-        fprintf(fw,"经度 :%s\n",lat2);
+        fprintf(fw,"经度 :%s,",lat2);
         for(i=0;i<6;i++)
         {
             speed[i]=str1[i+39];
         }
         speed[5]='\0';
         printf("地面速率 :%s节\n",speed);
-        fprintf(fw,"地面速率 :%s节\n",speed);
+        fprintf(fw,"地面速率 :%s节,",speed);
         for(i=0;i<6;i++)
         {
             deg[i]=str1[i+45];
         }
         deg[5]='\0';
         printf("地面航向 :%s度\n",deg);
-        fprintf(fw,"地面航向 :%s度\n",deg);
+        fprintf(fw,"地面航向 :%s度,",deg);
         for(i=0;i<7;i++)
         {
             time1[i]=str1[i+51];
         }
         time1[6]='\0';
         printf("utc日期 :%c%c日%c%c月%c%c年\n",time1[0],time1[1],time1[2],time1[3],time1[4],time[5]);
-        fprintf(fw,"utc日期 :%c%c日%c%c月%c%c年\n",time1[0],time1[1],time1[2],time1[3],time1[4],time[5]);
+        fprintf(fw,"utc日期 :%c%c日%c%c月%c%c年,",time1[0],time1[1],time1[2],time1[3],time1[4],time[5]);
         for(i=0;i<2;i++)
         {
             state[i]=str2[i+37];
         }
         state[1]='\0';
         printf("gps状态 :%s\n",state);
-        fprintf(fw,"gps状态 :%s\n",state);
+        fprintf(fw,"gps状态 :%s,",state);
         for(i=0;i<3;i++)
         {
             num[i]=str2[i+39];
         }
         num[2]='\0';
         printf("卫星数量 :%s个\n",num);
-        fprintf(fw,"卫星数量 :%s个\n",num);
+        fprintf(fw,"卫星数量 :%s个,",num);
         for(i=0;i<5;i++)
         {
             high[i]=str2[i+43];
